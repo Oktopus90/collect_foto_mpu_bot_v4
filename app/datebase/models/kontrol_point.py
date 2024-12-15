bc2dbc25b061ab2b96e406a4ad9e689438bb2fe7
Ivan <voitsik.ivan@yadex.ru>
2024-12-15 23:33:08 +0300

    Добавлена модель с основными паолями

--- app/datebase/models/kontrol_point.py
+++ app/datebase/models/kontrol_point.py
@@ -1,4 +1,5 @@
 from .base import AbstractModelForTime
+from sqlalchemy import BigInteger, Column, String, Integer, Float
 
 
 class KontrolPoint(AbstractModelForTime):
@@ -7,11 +8,41 @@ class KontrolPoint(AbstractModelForTime):
     Модель содержит:
     - number: Номер
     - adres: Адрес
-    - coord: Координаты
-    - discription: Описание
+    - latitude: Широта
+    - longitude: Долгота
+    - district: Район
     - question: Предварительный вопрос
+    - discription: Описание
     - comments: Общий коментарий
     - photo: Фотография
-    - district: Район
     """
-    
+
+    number = Column(
+        Integer,
+        unique=True,
+        autoincrement=True,
+    )
+    adres = Column(
+        String(length=255),
+    )
+    latitude = (
+        Float()
+    )
+    longitude = (
+        Float()
+    )
+    district = Column(
+        String(length=255),
+    )
+    question = Column(
+        String(length=255),
+    )
+    discription = Column(
+        String(length=255),
+    )
+    comments = Column(
+        String(length=255),
+    )
+    photo = Column(
+        String(length=255),
+    )
