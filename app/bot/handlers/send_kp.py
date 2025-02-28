305f70eb6b0e36584deac9ad464e0575ac4f4fd3
Ivan 402 <pugovizza@gmail.com>
2025-02-28 12:43:16 +0300

    add state

--- app/bot/handlers/send_kp.py
+++ app/bot/handlers/send_kp.py
@@ -6,6 +6,23 @@ from utils.logger import get_logger
 logger = get_logger(__name__)
 START_BUTTON_SEND_KP = constants.WELCOM_MENY[0]['Name']
 
+user_state = {}
+states = [
+    'start',
+    'adress',
+    'coord',
+    'age_category',
+    'question',
+    'discription',
+    'comments',
+    'photos',
+]
+
+def chek_state(message: Message, chek_state: str) -> bool:
+    """Проверка сотсояния и id."""
+    return user_state.get(message.chat.id, 0) == chek_state
+
+    
 
 @bot.message_handler(func=lambda message: message.text == START_BUTTON_SEND_KP)
 async def start_send_kp(message: Message) -> None:
@@ -13,4 +30,19 @@ async def start_send_kp(message: Message) -> None:
     await bot.send_message(
         message.chat.id,
         "Приступаем к сбору данных",
+    )
+    user_state[message.chat.id] = states[0]
+    await bot.send_message(
+        message.chat.id,
+        f"Ваше состояние {user_state[message.chat.id]}",
+    )
+    user_state[message.chat.id] = states[1]
+
+@bot.message_handler(func=lambda message: chek_state(message, 'adress'))
+async def adress_send_kp(message: Message) -> None:
+    """Начало сбора данных об КП."""
+    user_state[message.chat.id] = states[0]
+    await bot.send_message(
+        message.chat.id,
+        f"Ваше состояниеssds {user_state[message.chat.id]}",
     )
